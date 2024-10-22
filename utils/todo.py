62dd99c1a0fc3244f1c2990c0b2935c00fabd45e
Ashish Lathkar <63287421+Ashishlathkar77@users.noreply.github.com>
2024-10-21 23:26:25 -0400

    Update todo.py

--- utils/todo.py
+++ utils/todo.py
@@ -1,26 +1,27 @@
-# todo.py
-
 # Function to manage a to-do list
 to_do_list = []
 
-def add_task(task):
-    to_do_list.append(task)
-    return f"Task added: {task}"
-
-def view_tasks():
-    return to_do_list if to_do_list else "To-do list is empty."
-
-def remove_task(task):
-    if task in to_do_list:
-        to_do_list.remove(task)
-        return f"Task removed: {task}"
+def manage_todo_list(action, task=None, task_to_update=None):
+    if action == "add":
+        if task:
+            to_do_list.append(task)
+            return f"Task added: {task}"
+        else:
+            return "No task provided."
+    elif action == "view":
+        return "\n".join(to_do_list) if to_do_list else "To-do list is empty."
+    elif action == "remove":
+        if task in to_do_list:
+            to_do_list.remove(task)
+            return f"Task removed: {task}"
+        else:
+            return "Task not found."
+    elif action == "update":
+        if task in to_do_list and task_to_update:
+            index = to_do_list.index(task)
+            to_do_list[index] = task_to_update
+            return f"Task updated: {task} to {task_to_update}"
+        else:
+            return "Task not found or no new task provided."
     else:
-        return "Task not found."
-
-def update_task(old_task, new_task):
-    if old_task in to_do_list:
-        index = to_do_list.index(old_task)
-        to_do_list[index] = new_task
-        return f"Task updated: {old_task} to {new_task}"
-    else:
-        return "Task not found."
+        return "Invalid action."
